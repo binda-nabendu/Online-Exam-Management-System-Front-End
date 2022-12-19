9e77cbc1a781fb00f69754123bf6b905cb367b68
BindaNabendu <bindanabendu@gmail.com>
2022-12-19 22:06:49 +0600

    add necessery folder and organized for further process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicDashboardComponent } from './all-component/public/public-dashboard/public-dashboard.component';
+import { HomeComponent } from './all-component/public/home/home.component';
+import { TncComponent } from './all-component/public/tnc/tnc.component';
+import { AboutUsComponent } from './all-component/public/about-us/about-us.component';
+import { FaqComponent } from './all-component/public/faq/faq.component';
+import { LoginComponent } from './all-component/public/login/login.component';
+import { RegistrationComponent } from './all-component/public/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicDashboardComponent,
+    HomeComponent,
+    TncComponent,
+    AboutUsComponent,
+    FaqComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
