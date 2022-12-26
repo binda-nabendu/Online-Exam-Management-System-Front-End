33605c5534716cd6cf0b5f7855343455fd747b62
sourav <dsourav049@gmail.com>
2022-12-26 22:43:33 +0600

    Registation error solve

--- src/app/all-component/public/home/home.component.ts
+++ src/app/all-component/public/home/home.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css','./assets/css/fontawesome.css','./vendor/bootstrap/css/bootstrap.min.css'],
+  styleUrls: ['./home.component.css','../../../../assets/css/fontawesome.css','./vendor/bootstrap/css/bootstrap.min.css'],
 })
 export class HomeComponent {
   constructor(private router: Router){
