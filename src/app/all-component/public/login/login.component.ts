a2b1999bc85678e6d397f26b61c2f6558211e7aa
BindaNabendu <bindanabendu@gmail.com>
2022-12-26 11:31:05 +0600

    add material module

--- src/app/all-component/public/login/login.component.ts
+++ src/app/all-component/public/login/login.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MaterialModule} from "../../../../Material-Module";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  imports: [CommonModule,MaterialModule],
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
