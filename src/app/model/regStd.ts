9b513c0745d35ccf29a526abef2e216560b802b7
BindaNabendu <bindanabendu@gmail.com>
2022-12-31 16:56:45 +0600

    add regstd regtec model and add reg for std and tec in service module

---
+++ src/app/model/regStd.ts
@@ -0,0 +1,14 @@
+export interface regStd{
+  nid: string,
+  userName: string,
+  fatherName: string,
+  motherName: string,
+  gender: string,
+  contactNo: string,
+  email: string,
+  dob: string,
+  address: string,
+  password: string,
+  deptId: string,
+  semester: string
+}
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {regStd} from "../model/regStd";
+import {regTec} from "../model/regTec";
 
 
 @Injectable({
@@ -14,5 +16,11 @@ export class UserService {
   }
   getFaq(){
     return this.httpClient.get(this.url+"public/faq");
+  }
+  submitStdReg(student: regStd){
+    return this.httpClient.post(this.url+"public/request-to-join-as-student", student);
+  }
+  submitTecReg(teacher: regTec){
+    return this.httpClient.post(this.url+"public/request-to-join-as-teacher", teacher);
   }
 }
