d66ccd793bef9d03c61cb2ad7caf4e3a912fd9fd
BindaNabendu <bindanabendu@gmail.com>
2023-01-01 01:48:59 +0600

    fully re-structured of registration teacher form which can handle from backword

--- src/app/all-component/public/registration-teacher/registration-teacher.component.ts
+++ src/app/all-component/public/registration-teacher/registration-teacher.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -6,19 +8,36 @@ import {Router} from "@angular/router";
   templateUrl: './registration-teacher.component.html',
   styleUrls: ['./registration-teacher.component.css']
 })
-export class RegistrationTeacherComponent {
+export class RegistrationTeacherComponent implements OnInit{
   constructor(private router: Router) {
+  }
+  ngOnInit(): void {
+
   }
   goToRegStdPage(){
     this.router.navigate(['public-dashboard/register-student']);
   }
-  registerTeacher(teacher:any){
+  regTecForm = new FormGroup({
+    nid: new FormControl('', Validators.required),
+    userName: new FormControl('', Validators.required),
+    fatherName: new FormControl('', Validators.required),
+    motherName: new FormControl('', Validators.required),
+    gender: new FormControl('2',Validators.required),
+    contactNo: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+    dob:new FormControl({ provide: MAT_DATE_LOCALE, useValue: 'en-GB' },Validators.required),
+    address:new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    eduQualification:new FormControl('',Validators.required),
+    expertise:new FormControl('',Validators.required)
+  });
+  registerTeacher(){
 
   }
   signup(){
     this.router.navigate(['public-dashboard/login']);
   }
-  backlogin(){
+  backLogin(){
     this.router.navigate(['public-dashboard/login']);
   }
 }
