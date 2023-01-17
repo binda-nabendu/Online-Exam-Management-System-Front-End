71554bc1a2775323b25d5f8d6314751d453b3b93
sourav <dsourav049@gmail.com>
2023-01-17 14:12:23 +0600

    build student and teacher dashboard add routing path of them

--- src/app/all-component/teacher/teacher-dashboard/teacher-dashboard.component.ts
+++ src/app/all-component/teacher/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-teacher-dashboard',
   templateUrl: './teacher-dashboard.component.html',
   styleUrls: ['./teacher-dashboard.component.css']
 })
-export class TeacherDashboardComponent {
+export class TeacherDashboardComponent implements OnInit{
+
+  @HostBinding('class.navbar-opened') navbarOpened = false;
+  @HostBinding('class.navbar-opened') sidebarOpened = false;
+  constructor() {
+  }
+  ngOnInit(): void {
+    this.username = "Acharjha Chanakkaya"
+  }
+  username:any;
+  li1=false; li2=false; li3=false; li4=false; li5= false;
+
+  toggleSidebar() {
+    this.sidebarOpened = !this.sidebarOpened;
+  }
+  // navbarOpened: any;
 
 }
--- src/app/all-component/teacher/teacher-routing.module.ts
+++ src/app/all-component/teacher/teacher-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TeacherDashboardComponent} from "./teacher-dashboard/teacher-dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "teacher-dashboard", component: TeacherDashboardComponent,
+    children:[
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
