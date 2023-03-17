c7df6d65c5956fe630d1f1d1d0fd34808937084f
BindaNabendu <bindanabendu@gmail.com>
2023-03-17 22:41:07 +0600

    fix bug in home component ts file css

--- src/app/all-component/public/home/home.component.ts
+++ src/app/all-component/public/home/home.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css','../../../../../../Nabendu_info_Personal/src/assets/Font/fontawesome.css','./vendor/bootstrap/css/bootstrap.min.css'],
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   constructor(private router: Router){
